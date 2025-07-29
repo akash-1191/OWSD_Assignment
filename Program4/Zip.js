e32c79f4a560396ab02702779274306bf5e422b4
Akash Maurya <158453744+akash-1191@users.noreply.github.com>
2025-07-29 13:57:45 +0530

    Delete Program4 directory
    s

--- Program4/Zip.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const Zlib = require("zlib");
-
-const input = fs.createReadStream("sample.txt");
-const output = fs.createWriteStream("sample.txt.gz");
-
-input.pipe(Zlib.createGzip()).pipe(output);
-
-console.log("file zipped!!");
