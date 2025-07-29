a5d7cabc5e35f62154dfc83f2222230f31a12215
Akash Maurya <158453744+akash-1191@users.noreply.github.com>
2025-07-29 13:58:20 +0530

    Delete Program7 directory
    a

--- Program7/app.js
+++
@@ -1,7 +0,0 @@
-const fetchData = async () => {
-    const res = await fetch('https://www.google.com');
-    const txt = await res.text();
-    console.log(txt.substring(0, 400));
-};
-
-fetchData();
