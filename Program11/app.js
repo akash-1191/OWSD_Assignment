73d15bf34333720bc7e61923eace0b48a53bd560
Akash Maurya <158453744+akash-1191@users.noreply.github.com>
2025-07-29 13:56:59 +0530

    Delete Program11 directory
    1

--- Program11/app.js
+++
@@ -1,8 +0,0 @@
-function isEven(num) {
-    if (typeof num !== 'number') {
-        throw new TypeError('Input must be a number');
-    }
-    return num % 2 === 0;
-}
-
-module.exports = isEven;
--- Program11/test.js
+++
@@ -1,6 +0,0 @@
-const utils = require('./app');
-
-console.log(utils.capitalize('hello'));
-console.log(utils.reverse('hello'));
-console.log(utils.isPalindrome('nodejs'));
-console.log(utils.isPalindrome('hello'));
